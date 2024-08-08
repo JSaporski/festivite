16db0f61fa6f32013a75ed1593a1b6590834c5c1
Azempresas7 <114088131+Azempresas7@users.noreply.github.com>
2024-08-07 21:22:19 -0300

    Add the final personalization inputs, such as style (dark/light theme switch) and upload file

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change the upload file name
+const thumbnail = document.querySelector('.thumbnail')
+const fileNameEl = document.querySelector('.file-name')
+
+thumbnail.addEventListener('input', () => {
+  const fileName = thumbnail.files[0].name
+  fileNameEl.innerText = fileName
+  fileNameEl.classList.add('uploaded')
+})
